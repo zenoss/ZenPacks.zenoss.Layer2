9be492a4271fee0d5ace50d9143047119a0dc22a
Volodymyr Sergeyev <vova.sergeyev@gmail.com>
2014-07-08 15:54:17 +0300

    Modeler plugin changed to include both CDP and LLDP

--- ZenPacks/zenoss/Layer2/modeler/plugins/zenoss/snmp/CDPLLDPDiscover.py
+++ ZenPacks/zenoss/Layer2/modeler/plugins/zenoss/snmp/CDPLLDPDiscover.py
@@ -9,21 +9,19 @@
 ######################################################################
 
 
-__doc__ = """CDP
+__doc__ = """CDP and LLDP
 
-Gather neighbour switches information of Cisco Discovery Protocol from SNMP,
-and create DMD interface objects
+Gather neighbour switches information of Cisco Discovery Protocol and
+Link Layer Discovery Protocol from SNMP, and create DMD interface objects
 
 """
 
-import re
-
-from Products.ZenUtils.Utils import cleanstring, unsigned, prepId
+from Products.ZenUtils.Utils import prepId
 from Products.DataCollector.plugins.CollectorPlugin import SnmpPlugin, GetTableMap
 
-class CDP(SnmpPlugin):
+class CDPLLDPDiscover(SnmpPlugin):
     """
-    Map CDP entries to DMD 'NeighbourSwitch' objects
+    Map CDP/LLDP entries to DMD 'NeighbourSwitch' objects
     """
     order = 100
     compname = ""
@@ -31,7 +29,7 @@ class CDP(SnmpPlugin):
     modname = "ZenPacks.zenoss.Layer2.NeighbourSwitch"
 
     snmpGetTableMaps = (
-        # Extended interface information.
+        # CDP cache entries
         GetTableMap('cdpCacheEntry', '.1.3.6.1.4.1.9.9.23.1.2.1.1',
                 {
                     '.1': 'cdpCacheIfIndex',
@@ -42,6 +40,15 @@ class CDP(SnmpPlugin):
                     '.11': 'cdpCacheNativeVLAN',
                     '.17': 'cdpCacheSysName',
                     '.23': 'cdpCachePhysLocation'
+                }
+        ),
+        # LLDP remote systems entries
+        GetTableMap('lldpRemEntry', '.1.0.8802.1.1.2.1.4.1.1',
+                {
+                    '.7': 'lldpRemPortId',
+                    '.8': 'lldpRemPortDesc',
+                    '.9': 'lldpRemSysName',
+                    '.10': 'lldpRemSysDesc'
                 }
         ),
     )
@@ -56,6 +63,8 @@ class CDP(SnmpPlugin):
         
         log.debug("%s tabledata = %s", device.id, tabledata)
         rm = self.relMap()
+
+        # CDP data
         for idx, data in tabledata.get("cdpCacheEntry").items():
             title = data.get('cdpCachePlatform', '')
             if title:
@@ -69,4 +78,22 @@ class CDP(SnmpPlugin):
                     'location': data.get('cdpCachePhysLocation', '')
                     })
             rm.append(om)
+
+        # LLDP data
+        for idx, data in tabledata.get("lldpRemEntry").items():
+            om_id = prepId("lldp_{}".format(idx))
+            port = data.get('lldpRemPortDesc', '') \
+                or data.get('lldpRemPortId', '')
+            title = data.get('lldpRemSysName', '') \
+                or 'LLDP entry {}'.format(om_id)
+
+            if title:
+                om = self.objectMap({
+                    'id': om_id,
+                    'title': title,
+                    'description': data.get('lldpRemSysDesc', ''),
+                    'device_port': port
+                    })
+            rm.append(om)
+
         return rm
