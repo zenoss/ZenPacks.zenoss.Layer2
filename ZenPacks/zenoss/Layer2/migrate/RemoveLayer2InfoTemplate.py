e58d62a219d91ebd4d8e6e08ba1150fd351d4fd5
ssbunyk <tbunyk@softserveinc.com>
2015-10-20 11:16:38 +0300

    removed unused code

--- ZenPacks/zenoss/Layer2/migrate/RemoveLayer2InfoTemplate.py
+++ ZenPacks/zenoss/Layer2/migrate/RemoveLayer2InfoTemplate.py
@@ -7,12 +7,7 @@
 #
 ##############################################################################
 
-"""Replace Layer2Info template with ClientMACs modeler plugin.
-
-This migration script finds all device classes and devices that have the
-Layer2Info monitoring template bound. It then unbinds the monitoring
-template and adds the zenoss.snmp.ClientMACs modeler plugin instead.
-"""
+"""Remove Layer2Info template """
 
 import logging
 
@@ -21,12 +16,6 @@ from Products.ZenModel.ZenPack import ZenPackMigration
 
 
 log = logging.getLogger('zen.Layer2')
-TEMPLATES_ZPROPERTY = 'zDeviceTemplates'
-MODELER_ZPROPERTY = 'zCollectorPlugins'
-
-LAYER2INFO_TEMPLATE = 'Layer2Info'
-CLIENTMACS_PLUGIN = 'zenoss.snmp.ClientMACs'
-
 
 class RemoveLayer2InfoTemplate(ZenPackMigration):
 
@@ -34,4 +23,4 @@ class RemoveLayer2InfoTemplate(ZenPackMigration):
 
     def migrate(self, pack):
         log.info('Removing Layer2Info template')
-        pack.dmd.Devices.rrdTemplates._delObject('Layer2Info')
+        pack.dmd.Devices.rrdTemplates._delObject('Layer2Info', suppress_events=True)
