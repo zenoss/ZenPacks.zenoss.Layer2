zenoss/ZenPacks.zenoss.Layer2

commit c400aaa92ee728e215a0cd3bbb41dc57f3ea56ae

Rewrite Redis backend to fix inconsistencies
This is a big update that replaces the "catalog" Redis backend with a
redesigned Redis backend. The existing Redis backend has a Zope
catalog-like API in front of it because the original backend used a Zope
catalog, and when that was found not to be suitable, it was found to be
less invasive to keep the catalog-like API in front of a Redis backend.
This update does away with the catalog-like API since it isn't
necessary, and the change was going to be invasive anyway.

The reason for replacing the backend is to deal with a problem of one
connections provider overwriting connection information from other
connections providers. The design had no concept or attribution or
origination for the data. The new design has origination at its heart.
Each connection provider is identified as a "origin", and every bit
connection data it contributes is tagged to that origin. Only when a bit
of data has no origin tags left can be be removed.

The "connections_catalog" module has been entirely removed. It has been
replaced by the "connections" module. The "connections" module is a thin
API used by the rest of the ZenPack to read and write connections data.
Most of the work is done by the "graph" module which is only used by the
"connections" module.

Fixes ZEN-25720, ZEN-25724, ZEN-25950.

ZenPacks/zenoss/Layer2/network_tree.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (C) Zenoss, Inc. 2007, 2014, 2015 all rights reserved.
+# Copyright (C) Zenoss, Inc. 2007, 2014-2016, all rights reserved.
 #
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
@@ -12,35 +12,24 @@ Contains function get_connections_json, which returns JSON string with
 links and nodes of network map, for d3.js to render.
 '''
 
-import copy
 import json
-from functools import partial
-import re
-
 import logging
+import re
 
 from zExceptions import NotFound
 
 from Products.Zuul import getFacade
 from zenoss.protocols.services.zep import ZepConnectionError
 
-from .connections_catalog import CatalogAPI
+from . import connections
 from .edge_contraction import contract_edges
 
 log = logging.getLogger('zen.Layer2')
 
 # TODO: To find optimal batch size values.
 ZEP_BATCH_SIZE = 400
-CATALOG_BATCH_SIZE = 400
 
 MAX_NODES_COUNT = 2000
-
-
-class StopTraversing(Exception):
-
-    """Raised when max nodes count is reached to stop network map generation."""
-
-    pass
 
 
 def get_connections_json(
@@ -85,242 +74,87 @@ def serialize(*args, **kwargs):
         return serialize(kwargs)
 
 
-SEVERITY_TO_COLOR = {
-    1: 'severity_debug',
-    2: 'severity_info',
-    3: 'severity_warning',
-    4: 'severity_error',
-    5: 'severity_critical'}
-
-
-def _convert_severity_to_color(severity):
-    return SEVERITY_TO_COLOR.get(severity, 'severity_none')
-
-
 def get_connections(rootnode, depth=1, layers=None):
-    zport = rootnode.zport
-    cat = CatalogAPI(zport)
+    # Include layer2 if any VLANs are selected.
+    layers = set(layers) or set()
+    if "layer2" not in layers and any((l.startswith("vlan") for l in layers)):
+        layers.add(u"layer2")
+
+    g = connections.networkx_graph(
+        root=rootnode.getPrimaryId(),
+        layers=layers,
+        depth=depth)
 
     nodes = []
-    links = {}
-    colors = {}  # mapping from pair of nodes to their layers
-    nodenums = {}
-    uuids = []
+    node_indexes = {}
+    links = []
+    reduced = False
 
-    # VLAN -> VLAN, Layer2 (search by vlan,
-    # but include also vlan unaware sections of network)
-    # Layer2 -> Layer2      (no search by vlans)
-    # VLAN, Layer2 -> Layer2 (no search by vlans)
-    if layers:
-        # copy so we not mutate function argument
-        layers = map(str, layers)  # and also coerce to str (unicode happens)
-        if 'layer2' in layers:
-            layers = [l for l in layers if not l.startswith('vlan')]
-        else:
-            if any((l.startswith('vlan') for l in layers)):
-                layers.append('layer2')
+    for i, node in enumerate(g.nodes()):
+        if i > MAX_NODES_COUNT:
+            reduced = True
+            break
 
-    def add_node(n):
-        if len(nodes) >= MAX_NODES_COUNT:
-            raise StopTraversing()
+        node_indexes[node] = i
+        adapter = NodeAdapter(node=str(node), dmd=rootnode.dmd)
+        nodes.append({
+            "name": adapter.titleOrId(),
+            "image": adapter.getIconPath(),
+            "path": adapter.get_link(),
+            "color": "severity_none",
+            "highlight": adapter.id == rootnode.id,
+            "important": adapter.important,
+            "uuid": adapter.getUUID(),
+            })
 
-        if n.id in nodenums:
-            return
+    for source, target, data in g.edges(data=True):
+        if source not in node_indexes or target not in node_indexes:
+            # This edge is missing one of its terminating nodes, and is
+            # therefore invalid. This could happen if we limited the
+            # nodes due to MAX_NODES_COUNT.
+            continue
 
-        nodenums[n.id] = len(nodes)
-        record = dict(
-            name=n.titleOrId(),
-            image=n.getIconPath(),
-            path=n.get_link(),
-            color='severity_none',
-            highlight=n.id == rootnode.id,
-            important=n.important,
-        )
-        nodes.append(record)
+        links.append({
+            "source": node_indexes[source],
+            "target": node_indexes[target],
+            "color": list(data.get("layers", []))
+            })
 
-        uuid = n.getUUID()
-        if n.important and uuid:
-            uuids.append((uuid, record))
+    # Add severity color to nodes.
+    color_nodes(nodes)
 
-    def add_link(a, b):
-        color = colors[(a.get_path(), b.get_path())]
-        s = nodenums[a.id]
-        t = nodenums[b.id]
+    return {
+        "nodes": nodes,
+        "links": links,
+        "reduced": reduced,
+        }
 
-        key = tuple(sorted([s, t]))
-        if key in links:
-            if s == links[key]['source']:
-                return  # already added
-            else:
-                links[key]['directed'] = False
-                return
 
-        links[key] = dict(
-            source=s,
-            target=t,
-            directed=True,
-            color=color,
-        )
-
-    adapt_node = partial(NodeAdapter, dmd=zport.dmd)
-
-    visited = set()
-
-    def get_connections(nodes, depth):
-        """ Depth-first search of the network tree emanating from nodes """
-        if depth == 0:
-            return set()
-
-        adapted_nodes = {}
-        for node in nodes:
-            adapted_node = adapt_node(node)
-            if adapted_node.id in visited:
-                continue
-            visited.add(adapted_node.id)
-            adapted_nodes[adapted_node.get_path()] = adapted_node
-
-        if not adapted_nodes:
-            return set()
-
-        # leafs of current node in graph
-        impacted = get_impacted(adapted_nodes.keys())
-        impactors = get_impactors(adapted_nodes.keys())
-
-        related = copy.deepcopy(impacted)
-        for node_path, links in impactors.iteritems():
-            related[node_path] = set(related.get(node_path, [])) | set(links)
-
-        # some of leaf may contain a component (usualy IpInterface) uid
-        # prefixed with asterix (!)
-        for node_path, links in related.iteritems():
-            adapted_node = adapted_nodes[node_path]
-            for link_node in links:
-                if this_is_link(link_node):
-                    adapted_node.link = link_node[1:]
-                    break
-
-        for node in adapted_nodes.itervalues():
-            add_node(node)
-
-        nodes_to_check = set()
-        for node_path, links in related.iteritems():
-            adapted_node = adapted_nodes[node_path]
-
-            interesting_links = [link_node for link_node in links
-                                 if not this_is_link(link_node)]
-
-            reverse_connections = get_reverse_connected(interesting_links)
-
-            for link_node in interesting_links:
-                link_adapted_node = adapt_node(link_node)
-                # need to check for uid in leafs before adding node to graph
-                # as next time it will be skipped due to optimization
-                for node_b in reverse_connections.get(link_node, []):
-                    if this_is_link(node_b):
-                        link_adapted_node.link = node_b[1:]
-                        break
-
-                add_node(link_adapted_node)
-                if link_node in impacted.get(node_path, []):
-                    add_link(adapted_node, link_adapted_node)
-                if link_node in impactors.get(node_path, []):
-                    add_link(link_adapted_node, adapted_node)
-
-                nodes_to_check.add(link_node)
-
-        return nodes_to_check
-
-    def get_reverse_connected(nodes):
-        q = dict(connected_to=nodes)
-        if layers:
-            q['layers'] = layers
-
-        result = {}
-        for b in cat.search(**q):
-            result.setdefault(b.entity_id, []).append(b.connected_to)
-
-        return result
-
-    def connection_not_in_this_vlans(edge, filter_layers):
-        return (
-            # check that we filter by vlans at all
-            any((l.startswith('vlan') for l in filter_layers))
-            # and check that this edge is vlan-aware (has some vlans)
-            and any((l.startswith('vlan') for l in edge.layers))
-            # all of the vlans of edge are not vlans we are interested in
-            and all((
-                l not in filter_layers
-                for l in edge.layers
-                if l.startswith('vlan')
-            ))
-        )
-
-    def get_impacted(nodes):
-        q = dict(entity_id=nodes)
-        if layers:
-            q['layers'] = layers
-
-        result = {}
-        for b in cat.search(**q):
-            if connection_not_in_this_vlans(b, layers):
-                continue
-            for c in b.connected_to:
-                colors[(b.entity_id, c)] = b.layers
-                result.setdefault(b.entity_id, []).append(c)
-
-        return result
-
-    def get_impactors(nodes):
-        q = dict(connected_to=nodes)
-        if layers:
-            q['layers'] = layers
-        result = {}
-        for b in cat.search(**q):
-            if connection_not_in_this_vlans(b, layers):
-                continue
-            colors[(b.connected_to, b.entity_id)] = b.layers
-            result.setdefault(b.entity_id, []).append(b.connected_to)
-
-        return result
-
-    def this_is_link(node):
-        if isinstance(node, str) and node[0] == "!":
-            return node[1:]
-
-    nodes_queue = [rootnode]
-    iteration_depth = depth
-    reduced_result = False
+def color_nodes(nodes):
+    """Add "color" property to each NodeAdapter in nodes."""
+    zep = getFacade("zep")
+    nodes_by_uuid = {x["uuid"]: x for x in nodes if x["important"]}
     try:
-        while nodes_queue:
-            nodes_to_check = set()
-            for pos in xrange(0, len(nodes_queue), CATALOG_BATCH_SIZE):
-                nodes_to_check.update(
-                    get_connections(nodes_queue[pos:pos + CATALOG_BATCH_SIZE],
-                                    iteration_depth))
+        for uuids_chunk in chunks(nodes_by_uuid.keys(), ZEP_BATCH_SIZE):
+            severities_by_uuid = zep.getWorstSeverity(uuids_chunk)
+            for uuid, severity in severities_by_uuid.iteritems():
+                if uuid in nodes_by_uuid:
+                    nodes_by_uuid[uuid]["color"] = {
+                        1: 'severity_debug',
+                        2: 'severity_info',
+                        3: 'severity_warning',
+                        4: 'severity_error',
+                        5: 'severity_critical',
+                        }.get(severity, 'severity_none')
 
-            nodes_queue = list(nodes_to_check)
-            iteration_depth -= 1
-    except StopTraversing:
-        reduced_result = True
+    except ZepConnectionError as e:
+        log.warning("Couldn't connect to ZEP: %s", e)
 
-    zep = getFacade('zep', zport)
 
-    try:
-        for pos in xrange(0, len(uuids), ZEP_BATCH_SIZE):
-            sevs = zep.getWorstSeverity(
-                [uuid for uuid, _ in uuids[pos:pos + ZEP_BATCH_SIZE]])
-
-            for uuid, record in uuids[pos:pos + ZEP_BATCH_SIZE]:
-                if uuid in sevs:
-                    record['color'] = _convert_severity_to_color(sevs[uuid])
-    except ZepConnectionError as err:
-        log.warning('Could not connect to ZEP: %s', err)
-
-    return dict(
-        links=links.values(),
-        nodes=nodes,
-        reduced=reduced_result
-    )
+def chunks(s, n):
+    """Generate lists of size n from iterable s."""
+    for chunk in (s[i:i + n] for i in range(0, len(s), n)):
+        yield chunk
 
 
 class NodeAdapter(object):
@@ -341,14 +175,6 @@ class NodeAdapter(object):
             return self.node.getNetworkName()
         elif hasattr(self.node, 'id'):
             return self.node.id
-        else:
-            return self.node
-
-    def get_path(self):
-        if hasattr(self.node, 'macaddress'):
-            return self.node.macaddress
-        elif hasattr(self.node, 'getPrimaryUrlPath'):
-            return self.node.getPrimaryUrlPath()
         else:
             return self.node
 
@@ -393,24 +219,6 @@ class NodeAdapter(object):
             return self.node.getUUID()
 
         return None
-
-    def getColor(self):
-        summary = self.getEventSummary()
-        if summary is None:
-            return 'severity_none'
-        colors = 'critical error warning info debug'.split()
-        color = 'debug'
-        for i in range(5):
-            if summary[i][2] > 0:
-                color = colors[i]
-                break
-        return 'severity_%s' % color
-
-    def getEventSummary(self):
-        if not self.important:
-            return None
-        if hasattr(self.node, 'getEventSummary'):
-            return self.node.getEventSummary()
 
     @property
     def important(self):

README.mediawiki
@@ -218,45 +218,6 @@ installation.
 === Empty map/links for device ===
 In case index for certain device is broken, one may force zenmapper to reindex this specific device. Daemon should be runed with <code>--force</code> option.
 
-=== Debugging network map ===
-This ZenPack gathers information about connections on different layers
-of network, and displays it on network map.  If you see some connections
-that should not be there, or are missing, first you could check if network
-map is rendered from proper model - connections catalog.
-
-To do that, install <code>tabulate</code> python package
-(<code>pip install tabulate</code>), start <code>zendmd</code> shell and run:
-
-<syntaxhighlight lang=bash>
-    from ZenPacks.zenoss.Layer2.connections_catalog import CatalogAPI
-    cat = CatalogAPI(zport)
-
-    # example queries
-    cat.show_content() # show all catalog
-    cat.show_content(entity_id='/zport/dmd/Devices/Network/Switch/devices/192.168.1.1') # show only connections for given entity
-    cat.show_content(connected_to='/zport/dmd/Devices/Network/Switch/devices/192.168.1.1') # show what is connected to this entity
-    cat.show_content(layers=['layer2']) # show only connections on some layer
-</syntaxhighlight>
-
-If you don't see connections you expecting to see, you could check
-if <code>zenmapper</code> builds network map properly by running it with
-high verbosity:
-
-<syntaxhighlight lang=bash>
-zenmapper run -v10
-zenmapper run -v10 2> zenmapper.log # to redirect it to file
-</syntaxhighlight>
-
-Then you will see following log entries:
-
-<pre>
-2015-09-14 13:14:58,521 DEBUG zen.Layer2: Connection from ... to ... on layers ... added to connections_catalog
-</pre>
-
-When connections that reported by that log are erroneous, then probably there
-are errors in connections providers for your device, or in device model
-itself.
-
 === Layer2 forwarding table ===
 Let's discuss Layer2 connections in particular.
 

ZenPacks/zenoss/Layer2/connections.py
@@ -1,0 +1,196 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2016, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+"""Glue between Zenoss and more abstract graph, networkx, and redis modules."""
+
+# stdlib imports
+import functools
+
+# zenoss imports
+from Products.ZenUtils.guid.interfaces import IGlobalIdentifier
+
+# third-party imports
+import networkx
+import redis
+
+# zenpack imports
+from . import graph
+from .connections_provider import IConnectionsProvider
+
+# logging
+import logging
+LOG = logging.getLogger("zen.Layer2")
+
+# constants
+GRAPH_NAMESPACE = "g"
+LAYER2_LAYER = "layer2"
+LAYER2_NEIGHBOR_DEVICE_DEPTH = 3
+DEVICES_PREFIX = "/zport/dmd/Devices/"
+DEVICES_NETWORK_PREFIX = "/zport/dmd/Devices/Network/"
+
+
+def log_redis_errors(default=None):
+    """Log RedisError in decorated function and return default."""
+    def wrap(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except redis.RedisError as e:
+                LOG.exception("Redis Error: %s", e)
+
+                # Avoid problems with mutable default value.
+                if isinstance(default, set):
+                    return set()
+                elif isinstance(default, list):
+                    return []
+                elif isinstance(default, networkx.Graph):
+                    return networkx.Graph()
+                else:
+                    return default
+
+        return wrapper
+
+    return wrap
+
+
+@log_redis_errors(default=set())
+def get_layers():
+    """Return set of all known layers."""
+    g = graph.Graph(GRAPH_NAMESPACE)
+    return g.get_layers()
+
+
+@log_redis_errors(default=networkx.Graph())
+def networkx_graph(root, layers, depth=None):
+    """Return NetworkX graph of layers at depth starting from root."""
+    g = graph.Graph(GRAPH_NAMESPACE)
+    return g.networkx_graph(root, layers, depth=depth)
+
+
+@log_redis_errors(default=[])
+def get_neighbors(node, layers):
+    """Return list of all of nodes neighbors."""
+    g = graph.Graph(GRAPH_NAMESPACE)
+    return [target for _, target, _ in g.get_edges(node, layers)]
+
+
+@log_redis_errors(default=None)
+def get_device_by_mac(dmd, macaddress):
+    """Return first neighbor of macaddress that's a device."""
+    nxg = networkx_graph(macaddress, [LAYER2_LAYER], depth=1)
+    for node in nxg.nodes():
+        if node.startswith(DEVICES_PREFIX):
+            try:
+                return dmd.getObjByPath(str(node))
+            except Exception:
+                continue
+
+
+@log_redis_errors(default=[])
+def get_layer2_neighbor_devices(device):
+    """Generate devices that are layer2 neighbors of device."""
+    device_uid = device.getPrimaryId()
+    nxg = networkx_graph(
+        device_uid,
+        [LAYER2_LAYER],
+        depth=LAYER2_NEIGHBOR_DEVICE_DEPTH)
+
+    for node in nxg.nodes():
+        if node == device_uid:
+            # The device can't be its own neighbor.
+            continue
+
+        try:
+            yield device.getObjByPath(str(node))
+        except Exception:
+            continue
+
+
+@log_redis_errors(default=None)
+def add_node(node, force=False):
+    """Add node and all of its connections to the graph."""
+    guid = IGlobalIdentifier(node).getGUID()
+    origin = graph.Origin(GRAPH_NAMESPACE, guid)
+    last_changed = get_last_changed(node)
+
+    if not force and last_changed == origin.get_checksum:
+        # No need to do anything if we're up-to-date for this node.
+        return
+
+    edges = []
+    for connection in IConnectionsProvider(node).get_connections():
+        for connected_to in connection.connected_to:
+            edges.append((
+                connection.entity_id,
+                connected_to,
+                connection.layers))
+
+    origin.clear()
+    origin.add_edges(edges, last_changed)
+
+
+def is_switch(device):
+    """Return True if device is "switchy", and False if "hosty"."""
+    return device.getDeviceClassName().startswith("/Network/")
+
+
+def get_last_changed(node):
+    """Return string indicating last time node was changed.
+
+    The string doesn't have to necessarily have to represent time. It
+    just needs to be a high precision value that can be used to know
+    that if the value returned is different from a value returned at a
+    point in history, then the node has changed since then.
+
+    """
+    try:
+        # Prefer this because it should be available on devices, and is
+        # an "application" value that can even be updated within a
+        # transaction.
+        return node.getLastChange().micros()
+    except Exception:
+        pass
+
+    try:
+        # Fall back to this because all ZenModelRM objects (at least)
+        # have it. It's not as good because it's probably only the last
+        # time the object was committed. So if we call it twice within
+        # the same transaction before and after useful changes have
+        # occurred, we'll think that no useful changes have occurred.
+        return node.bobobase_modification_time().micros()
+    except Exception:
+        pass
+
+
+def get_status(dmd, node):
+    """Return False if node is "down". Otherwise return True."""
+    try:
+        obj = dmd.getObjByPath(node)
+    except Exception:
+        return True
+
+    try:
+        return IConnectionsProvider(obj).get_status()
+    except Exception:
+        return True
+
+
+@log_redis_errors(default=None)
+def clear():
+    """Clear all data for all origins."""
+    g = graph.Graph(GRAPH_NAMESPACE)
+    g.clear()
+
+
+@log_redis_errors(default=None)
+def compact(guids):
+    """Clear data from origins not listed in guids."""
+    g = graph.Graph(GRAPH_NAMESPACE)
+    g.compact(guids)

commit fa2dac525a69a15012ac4fe92016fe6f7d4cee10

Restore default map behavior to show all layers
Commit c400aaa regressed the default behavior of the network map to show
no layers instead of all layers. This resulted in a "No data." error
showing on all Network Map pages until the "Apply" button was clicked.

Fixes ZEN-26271.

ZenPacks/zenoss/Layer2/network_tree.py
@@ -76,7 +76,7 @@ def serialize(*args, **kwargs):
 
 def get_connections(rootnode, depth=1, layers=None):
     # Include layer2 if any VLANs are selected.
-    layers = set(layers) or set()
+    layers = set(layers) or connections.get_layers()
     if "layer2" not in layers and any((l.startswith("vlan") for l in layers)):
         layers.add(u"layer2")
 